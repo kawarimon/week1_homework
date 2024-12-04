c765b660853fd9c709b5a70a56905d50dd5d753b
kawarimon <gods.are.220121@gmail.com>
2024-12-04 12:53:39 +0900

    exercise A

---
+++ a-8.py
@@ -0,0 +1,10 @@
+users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
+
+for user in users_info:
+    name, age = user
+    print(f"Name: {name}, Age:{age}")
+
+# Name: Bob, Age: 79
+# Name: Tom, Age: 59
+# Name: Ken, Age: 61
+# print(f"Name: [name], Age:[age]") {}[]の使い分け要理解
